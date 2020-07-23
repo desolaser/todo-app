5adf440ee2ccd7792e98eb147d25099d61f7c615
Felipe Olavarria <178.felipe@gmail.com>
2020-07-23 11:19:44 -0400

    Send auth headers in requests

--- src/utils/ApolloClient.js
+++ src/utils/ApolloClient.js
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('tokens');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
